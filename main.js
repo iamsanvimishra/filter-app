246be400802e3cf5189b0636f7fc9c9f967b9005
iamsanvimishra <111888609+iamsanvimishra@users.noreply.github.com>
2023-04-06 16:41:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload()
+{
+}
+
+function setup(){
+ canvas=createCanvas(300,300);
+ canvas.center();
+
+}
+function take_snopshot(){
+    save('myFilerImage.png');
+}
+function draw(){  
+}
